6ab361f2441b07fec0c6d49d89ca7d2e41c6228a
chdaud78 <qkrri56@naver.com>
2020-12-10 18:01:20 +0900

    [TST]

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login">
+    <form action="">
+      <label for="uId">아이디</label>
+      <input type="text" id="uId" >
+      <label for="uPw">비밀번호</label>
+      <input type="text" id="uPw">
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LoginPage",
+  props: {
+
+  },
+  computed: {
+
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+</style>
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,7 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/LoginPage.vue";
 
-describe("HelloWorld.vue", () => {
+describe("LoginPage.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
